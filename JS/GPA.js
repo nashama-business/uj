582740c7fca64b98c3ea2fd36e43af583d72d4d7
nashama-business <nashama.business@gmail.com>
2025-03-21 22:28:30 +0300

    v2.102

--- JS/GPA.js
+++ JS/GPA.js
@@ -396,3 +396,21 @@ calculateButton.addEventListener('click', calculateGPA);
 resetButton.addEventListener('click', resetForm);
 
 updateCoursesUI();
+
+const navEl = document.querySelector('.nav');
+const hamburgerEl = document.querySelector('.hamburger');
+const navItemEls = document.querySelectorAll('.nav__item');
+
+hamburgerEl.addEventListener('click', () => {
+  navEl.classList.toggle('nav--open');
+  hamburgerEl.classList.toggle('hamburger--open');
+});
+
+navItemEls.forEach(navItemEl => {
+  navItemEl.addEventListener('click', () => {
+    navEl.classList.remove('nav--open');
+    hamburgerEl.classList.remove('hamburger--open');
+  });
+});
+
+
